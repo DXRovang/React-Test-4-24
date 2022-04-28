f7107243fe4e91eaa2ab33accb45f3c1213c0f31
Alexander Rovang <DXRovang@gmail.com>
2022-04-28 11:02:47 -0400

    plays around with hooks #3

--- src/Grandchild1.js
+++ src/Grandchild1.js
@@ -4,36 +4,57 @@ import {useState} from 'react'
 
 const Grandchild1 = ({characters}) => {
 
-  const names = {
-    1 : "james",
-    2 : "mike", 
-    3: "charles"
-  }
-
-  const [nebraska, setNebraska] = useState(["Nick", "Alex"])
-
   const [toggle, setToggle] = useState(false)
+  const [toggleDiv, setToggleDiv] = useState(false)
+  const [first, setFirst] = useState(["Nick", "Alex"])
 
   const handleClick = () =>{
-    console.log(nebraska)
-    setNebraska(["Alex", "Nick"])
-    console.log(nebraska)
+    setFirst(["Alex", "Nick"])
   }  
-  const myList = [characters]
-  const {id} = useParams()
-  console.log(myList)
+  const handleClickN = () =>{
+    setFirst(["Nick", "Alex"])
+  } 
+  // const names = {
+  //   1 : "james",
+  //   2 : "mike", 
+  //   3 : "charles"
+  // }
+
+  // const list = ["james", "dan", "charles"]
+  // const [n, setN] = useState(names)
+
+  // const myList = [characters]
+  // const {id} = useParams()
+  // console.log(myList)
   // debugger
   return ( 
     <div>
-    <div className={toggle ? "pink" : "blue"}>Hi!  Grandchild1</div>
-    <div>{names[id]}</div>
-    <button onClick={()=>setToggle(!toggle)}>Toggle</button>
-    <button onClick={handleClick}>Click me</button>
-    <div>{toggle}</div>
-    <div>
-      {toggle && (<div>Now you see me...</div>)}
-    </div>
-    {/* {myList ? myList[0].filter(character => character.id === id).map(c=>(<div key={c.id}>{c.name}</div>)):null } */}
+      <div className={toggle ? "pink border" : "blue border"}>
+        This div changes color
+        <div>
+          <button onClick={()=>setToggle(!toggle)}>Toggle Color</button>
+        </div>
+      </div>
+  
+      <div className="border">
+        <button onClick={()=>setToggleDiv(!toggleDiv)}>Toggle Div</button>
+        <div>
+          {toggleDiv && (<div>Now you see me...</div>)}
+        </div>
+      </div>
+
+      <div className="border">
+        <button onClick={handleClickN}>Nick is First</button>
+        <button onClick={handleClick}>Alex is First</button>
+        <div>{first.map(name=><div>{name}</div>)}</div>
+      </div>
+    {/* <div>{toggle}</div> */}
+
+    {/* <div>{names[id]}</div>
+    <div>{list.filter(n=> n.includes("m")).map(n=><div>{n}</div>)}</div>
+
+    <div>{Object.keys(names).filter(key=>(key===id)).map(n=>names[n])}</div> */}
+    {/* <div>{list.filter((key, index)=>index === id).map(i=><div>{i}</div>)}</div> */}
   </div>
    );
 }
